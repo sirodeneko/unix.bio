cf72e96a97c8652033f4e8eaee34a8db63d67fd2
sirodeneko <48661001+sirodeneko@users.noreply.github.com>
2020-07-22 22:53:39 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,25 +1,32 @@
 
 const BLOG = {
-  anthor: 'me',
-  title: 'siroblog',
-  description: 'Witt\'s blog',
-  summary: 'siro与neko',
-  language: 'zh-cn',//'en-us'或'zh-cn'
+  author: 'Witt',
+  title: 'Witt - unix.bio',
+  description:
+    "Hi, I'm Witt. A JavaScript full stack engineer, I'm creating apps and libs to help improve the developer experience.",
+  summary: 'JavaScript 工程师，对开发者体验感兴趣。',
+  language: 'zh-cn',
   latestLimit: 5,
-  
+
   labels: {
-    default: '文章',
+    default: '所有文章',
     latest: '最近',
     list: '所有',
   },
-  
-  email: 'siro-neko@qq.com',
-  github: 'sirodeneko',
-  twitter: '',
-  domain: 'xiaosiro.cn',
-  
-  googleAnalytics: '',
+
+  email: 'unix.bio@gmail.com',
+  github: 'unix',
+  twitter: 'echo_witt',
+  domain: 'unix.bio',
+
+  googleAnalytics: 'UA-110371817-3',
   cn: true,
+  enableViews: true,
+
+  layouts: {
+    pageWidth: '750px',
+    pageWidthMobile: '88vw',
+  },
 }
 
 export default BLOG
